e661210e96686f8ba407e828cc852a9ae6108f73
fischmaj <fischmaj@onid.oregonstate.edu>
2015-04-25 17:59:11 -0500

    updated gist.js-- new divsappear after the query, with links.

--- gist.js
+++ gist.js
@@ -2,7 +2,7 @@
 window.onload= function() {
   console.log('window.onload running');
   var settingsStr = localStorage.getItem('userSettings');
-  if (true){
+  if (settingsStr === null){
       settingsStr = { favsArray:[], allGistsArray:[]};
       localStorage.setItem('userSettings', JSON.stringify(settingsStr));
   }
@@ -32,7 +32,8 @@ function getGists(){
 
     console.log('pages =' + pages);
 
-    url = url+'?page='+pages+'&per_page=3';
+    url = url+'?pages='+pages+'&per_page=30';
+    console.log(url);
  
     //when the request is fulfilled
     req.onreadystatechange = function(){
@@ -48,16 +49,60 @@ function getGists(){
 	  localStorage.setItem('userSettings', JSON.stringify(userSettings));
 
 	  console.log(userSettings['allGistsArray']);
+
+          displayGists();
 	}
     }
     req.open('GET', url);
     req.send();
 
-    displayGists();
 }
 
 
 //
 function displayGists(){
 
+    console.log('in displayGists');
+
+    var userSettings = JSON.parse(localStorage.getItem('userSettings'));
+    var favorites = userSettings["favsArray"];
+    var favLength = favorites.length;
+    var gistList = userSettings["allGistsArray"];
+    var gistListLength = gistList.length;
+    var gistResultsElement = document.getElementById("Gist_search_results");
+    var favElement = document.getElementById("Gist_favorite_results");
+
+    for (var i = 0; i < favLength ; i++) {
+
+	for (var j = 0; j < gistListLength ; j++) {
+	    if (gistList[j]===favorites[i]){
+		gistList = gistList.slice(j, 1);
+		gistListLength = gistList.length;
+		j--;
+	    }
+	}
+    }
+    console.log(' through favorites stripping');
+
+    gistResultsElement.innerHTML = null;
+
+    for (var i = 0; i < gistListLength ; i ++){
+	console.log('trying to add element' + i + ' to page');
+	var resultDiv = document.createElement("div");
+	var para = document.createElement("P");
+	para.innerHTML = "description: " + gistList[i].description; 
+	if ((!gistList[i].description) || (gistList[i].description ==="")){
+	    para.innerHTML += '<p style="color:red:>NO DESCRIPTION</p>'; 
+	}
+	var linkUrl = gistList[i].url;
+	var link = document.createElement("a");
+	link.href = linkUrl;
+	link.title = "LINK";
+	link.innerHTML = "LINK";
+	resultDiv.appendChild(para);
+	resultDiv.appendChild(link);
+	resultDiv.setAttribute("id", "resultDiv");
+	gistResultsElement.appendChild(resultDiv);
+    }
+   
 }
