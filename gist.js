41490c16888fd64500d5b9d22a852ab6c87e2e1e
fischmaj <fischmaj@onid.oregonstate.edu>
2015-04-26 18:45:09 -0500

    fixed several bugs including favorites not removing when -favorite pressed.

--- gist.js
+++ gist.js
@@ -1,11 +1,15 @@
 
 window.onload= function() {
   console.log('window.onload running');
-  var settingsStr = localStorage.getItem('userSettings');
-  if (true){
-      settingsStr = { favsArray:[], allGistsArray:[]};
-      localStorage.setItem('userSettings', JSON.stringify(settingsStr));
+  var settings = JSON.parse(localStorage.getItem('userSettings'));
+  if (settings === null){
+      settings = { favsArray:[], allGistsArray:[]};
+      localStorage.setItem('userSettings', JSON.stringify(settings));
+  } else {
+      settings["allGistsArray"]=[];
+      localStorage.setItem('userSettings', JSON.stringify(settings));
   }
+  displayGists();
 }
 
 
@@ -16,45 +20,59 @@ window.onload= function() {
 //and displays
 function getGists(){
     console.log('in getGists');
-    var req = new XMLHttpRequest();
-    if (!req){
+    var req1 = new XMLHttpRequest();
+    var req2 = new XMLHttpRequest();
+    if (!req1  && !req2){
 	throw 'Unable to create HttpRequest.';
     }
     var url = 'https://api.github.com/gists/public';
-    var numElement =document.getElementsByName('numPerPage');
-    var pages = numElement[0].value;
-    if (pages > 5){
-	pages = 5;
-    } else if (pages < 1){
-	pages = 1;
-    }
-    numElement[0].value = pages;
 
-    console.log('pages =' + pages);
+    //load the local storage variable
+    var userSettings = JSON.parse(localStorage.getItem('userSettings'));
+    console.log (userSettings);
 
-    url = url+'?pages='+pages+'&per_page=30';
-    console.log(url);
+    //clear out the old gists in the gist array to prepare for reload
+    userSettings["allGistsArray"] = [];
  
     //when the request is fulfilled
-    req.onreadystatechange = function(){
-	if(req.readyState == 4){
+    req1.onreadystatechange = function(){
+	if(req1.readyState == 4){
 
 	  //load the XMLHttpRequest response into a variable 
 	  var gistList = JSON.parse(this.responseText);
-
-	  //load the local storage variable
-          var userSettings = JSON.parse(localStorage.getItem('userSettings'));
-	  console.log (userSettings);
-	  userSettings["allGistsArray"] = gistList;
+	  var previousGistList = userSettings["allGistsArray"];
+	  userSettings["allGistsArray"]=[].concat(previousGistList, gistList);
 	  localStorage.setItem('userSettings', JSON.stringify(userSettings));
 
-	  console.log(userSettings['allGistsArray']);
+	  console.log(userSettings['allGistsArray'].length);
 
           displayGists();
 	}
     }
-    req.open('GET', url);
-    req.send();
+
+    req2.onreadystatechange = function(){
+	if(req2.readyState == 4){
+
+	  //load the XMLHttpRequest response into a variable 
+	  var gistList = JSON.parse(this.responseText);
+	  var previousGistList = userSettings["allGistsArray"];
+	  userSettings["allGistsArray"]=[].concat(previousGistList, gistList);
+	  localStorage.setItem('userSettings', JSON.stringify(userSettings));
+
+	  console.log(userSettings['allGistsArray'].length);
+
+          displayGists();
+	}
+    }
+    //get the first 100 
+    req1.open('GET', (url+'?page=1&per_page=100'));
+    req1.send();
+
+    //Now do the same thing with the second page of 100 gists
+    console.log(url);
+    req2.open('GET', (url+'?page=2&per_page=100'));
+    req2.send();
+
 
 }
 
@@ -66,17 +84,33 @@ function displayGists(){
 
     var userSettings = JSON.parse(localStorage.getItem('userSettings'));
     var favorites = userSettings["favsArray"];
-    console.log(favorites);
     var favLength = favorites.length;
     var gistList = userSettings["allGistsArray"];
     var gistListLength = gistList.length;
     var gistResultsElement = document.getElementById("Gist_search_results");
     var favElement = document.getElementById("Gist_favorite_results");
+    var numPerPageElement = document.getElementsByName("numPerPage");
+    var numPerPage = numPerPageElement[0].value;
+
+    //ensure numPerPage is between 1 and 30
+    if ((numPerPage >30) || (numPerPage < 1)) { 
+	numPerPage = 30;
+	numPerPageElement[0].value =30;
+    }
+    var pageNumberElement = document.getElementsByName("page#");
+
+    //determine which page number is selected.
+    for (var j =0; j < pageNumberElement.length; j++){
+	if (pageNumberElement[j].checked){
+	    var pageNumber = j;
+	    break;
+	}
+    }
 
     for (var i = 0; i < favLength ; i++) {
 
 	for (var j = 0; j < gistListLength ; j++) {
-	    if (gistList[j] === favorites[i]){
+	    if (gistList[j].url === favorites[i].url){
 		gistList = gistList.slice(j, 1);
 		gistListLength = gistList.length;
 		j--;
@@ -87,14 +121,18 @@ function displayGists(){
     localStorage.setItem('userSettings', JSON.stringify(userSettings));
 
  
-    //Create a div for each GIST remaining in GIST array
-    gistResultsElement.innerHTML = null;
-    for (var i = 0; i < gistListLength; i++)  {
+    //Create a div for each GIST to be displayed and display 
+    if (gistList.length > 0){
+      gistResultsElement.innerHTML = null;
+      for (var i = ((pageNumber)*numPerPage);
+	 i < ((pageNumber+1)*numPerPage);
+	 i++)  {
 	var resultDiv = createNonFavGistDiv(gistList[i]);
 	gistResultsElement.appendChild(resultDiv);
+      }
     }
-
     //Create a div for each Element in the favs Array
+
     favElement.innerHTML = null;
     for (var j = 0; j < favLength; j++) {
 	console.log(favorites);
@@ -105,15 +143,21 @@ function displayGists(){
    
 }
 
+
+
+
 function createNonFavGistDiv(gistObject ) {
   var resultDiv = document.createElement("div");
   var para = document.createElement("P");
-  para.innerHTML = "description: " + gistObject.description; 
-  if ((!gistObject.description) || (gistObject.description ==="")) {
-    para.innerHTML += '<p style="color:red:>NO DESCRIPTION</p>'; 
-  }
   var linkUrl = gistObject.url;
   var link = document.createElement("a");
+
+  para.innerHTML = "description: " + gistObject.description; 
+  if ((!gistObject.description) || (gistObject.description ==="")
+      || (gistObject.description==null)) {
+    para.innerHTML += '<p style="color:red:>NO DESCRIPTION</p>'; 
+  }
+  para.innerHTML += '<br>'+linkUrl;
   link.href = linkUrl;
   link.title = "LINK";
   link.innerHTML = "LINK";
@@ -129,17 +173,24 @@ function createNonFavGistDiv(gistObject ) {
   return resultDiv;
 }
 
+
+
+
 function createFavDiv(gistObject ) {
-    console.log('In createFavDiv');
+  console.log('In createFavDiv');
   console.log(gistObject);
   var resultDiv = document.createElement("div");
   var para = document.createElement("P");
-  para.innerHTML = "description: " + gistObject.description; 
-  if ((!gistObject.description) || (gistObject.description ==="")) {
-    para.innerHTML += '<p style="color:red:>NO DESCRIPTION</p>'; 
-  }
   var linkUrl = gistObject.url;
   var link = document.createElement("a");
+
+  para.innerHTML = "description: " + gistObject.description; 
+  if ((!gistObject.description) || (gistObject.description ==="")
+      ||(gistObject.description==null)) {
+    para.innerHTML += '<p style="color:red:>NO DESCRIPTION</p>'; 
+  }
+  para.innerHTML += '<br>'+linkUrl;
+
   link.href = linkUrl;
   link.title = "LINK";
   link.innerHTML = "LINK";
@@ -154,6 +205,10 @@ function createFavDiv(gistObject ) {
   resultDiv.setAttribute("id", "resultDiv");
   return resultDiv;
 }
+
+
+
+
 
 function makeFavorite(elem){
     //Get and Parse all the local storage items
@@ -177,12 +232,39 @@ function makeFavorite(elem){
 	}
     }
 
-
-
-
     userSettings["allGistsArray"]= gistList;
     userSettings["favsArray"]= favsList;
     localStorage.setItem('userSettings', JSON.stringify(userSettings));
 
+    displayGists();
+}
+
+
+function removeFavorite(elem){
+   //Get and Parse all the local storage items
+    var userSettings = JSON.parse(localStorage.getItem('userSettings'));
+    var favsList = userSettings["favsArray"];
+    var favsListLength = favsList.length;
+   
+
+    //get the URL of the <a> tag in the calling DIV
+    var linkString = elem.parentNode.childNodes[1].href;
+    console.log('linkString');
+    //search the list of fav Gists for the one with that matches the
+    //caller.
+    for (var i = 0; i < favsListLength; i++){
+	if (favsList[i].url === linkString) {
+	    //if we've found the item, remove it from favsList and 
+	    //add it to gistList
+	    favsList.splice(i,1);
+	    break;
+	}
+    }
+
+    //store the arrays back in local storage
+    userSettings["favsArray"]= favsList;
+    localStorage.setItem('userSettings', JSON.stringify(userSettings));
+
+    //display the updated results.
     displayGists();
 }
